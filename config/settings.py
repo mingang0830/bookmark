d1ee008afb43129df0a5f8da26d9e23ae9e18084
mingyeong <mingang0830@gmail.com>
2021-08-09 14:38:52 +0900

    배프의 웹프로그래밍 북마크앱
    allowed host 설정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)u5g&y9gi*wg0@gvr@&p0@gdyu#%@^+g3!c!-*an#-&wy9y641'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
