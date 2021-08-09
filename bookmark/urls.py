6083160d642b516bd813285fbccb317355fa2fa6
mingyeong <mingang0830@gmail.com>
2021-08-09 12:57:53 +0900

    배프의 웹프로그래밍 북마크앱
    삭제 기능

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', BookmarkListView.as_view(), name='list'),
-    path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('', BookmarkListView.as_view(), name='bookmark_list'),
+    path('add/', BookmarkCreateView.as_view(), name='bookmark_create'),
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='bookmark_detail'),
+    path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='bookmark_update'),
+    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='bookmark_delete'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Bookmark
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 
@@ -19,3 +19,15 @@ class BookmarkCreateView(CreateView):
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
+
+
+class BookmarkDeleteView(DeleteView) :
+    model = Bookmark
+    success_url = reverse_lazy('bookmark_list')
+
